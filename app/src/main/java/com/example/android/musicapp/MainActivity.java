6730fefaa74fd312dad5affa5c00d9aad493f759
marcelo.rocha <marcelo.rocha@fpf.br>
2017-09-04 10:54:57 -0400

    Adding new activities

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the playlist category
+        TextView playlist = (TextView) findViewById(R.id.playlist);
+
+        // Find the View that shows the albums category
+        TextView albums = (TextView) findViewById(R.id.albums);
+
+        // Find the View that shows the search category
+        TextView search = (TextView) findViewById(R.id.search);
+
+        // Find the View that shows the store category
+        TextView store = (TextView) findViewById(R.id.store);
+
+        // Set a click listener on that View
+        playlist.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the playlist View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent playlistIntent = new Intent(MainActivity.this, PlaylistActivity.class);
+                startActivity(playlistIntent);
+            }
+        });
+
+        // Set a click listener on that View
+        albums.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the albums View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent albumsIntent = new Intent(MainActivity.this, AlbumsActivity.class);
+                startActivity(albumsIntent);
+            }
+        });
+
+        // Set a click listener on that View
+        search.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the search View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent searchIntent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(searchIntent);
+            }
+        });
+
+        // Set a click listener on that View
+        store.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the store View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent storeIntent = new Intent(MainActivity.this, StoreActivity.class);
+                startActivity(storeIntent);
+            }
+        });
     }
 }
